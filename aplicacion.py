3c872b38a0b0434547a16924cc605a2660b648f6
LuiggiLeandro23 <1813011665@untels.edu.pe>
2022-05-30 22:19:09 -0500

    2.i

---
+++ aplicacion.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 12:06:49 2022
+
+@author: 18130
+"""
+
+import gestion_archivos
+
+def menu():
+    print("*****MENU PRINCIPAL*****")
+    print("1. Crear Archivo")
+    print("2. Eliminar archivo")
+    print("3. Agregar contenido")
+    print("4. Mostrar contenido del archivo")
+    print("5.Salir")
+    
